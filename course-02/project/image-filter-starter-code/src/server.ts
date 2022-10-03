542a0f432ae0152239cfa7125ca66e39e1ac8651
M-AlMokadem <moh_skimo@hotmail.com>
2022-10-03 21:43:13 +0200

    adding files

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { filterImageFromURL, deleteLocalFiles } from './util/util';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
 
@@ -9,7 +9,7 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-
+  
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -30,34 +30,17 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  app.get("/filteredimage", async (req, res) => {
-
-    console.log(req.query);
-    const imgUrl: string = req.query.imgUrl;
-    if (!imgUrl) {
-      res.send("no image provided");
-    }
-
-    await filterImageFromURL(imgUrl).then((imgPath: string) => {
-      res.sendFile(imgPath);
-      res.on('finish', () => deleteLocalFiles([imgPath]));
-    });
-    // res.send(req.query)
-  });
-
-  app.get("/", async (req, res) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  });
+  
   // Root Endpoint
   // Displays a simple message to the user
-  app.get("/", async (req, res) => {
+  app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  });
-
+  } );
+  
 
   // Start the Server
-  app.listen(port, () => {
-    console.log(`server running http://localhost:${port}`);
-    console.log(`press CTRL+C to stop server`);
-  });
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
 })();
